541c901e435b9d5f09994ba98d9115407967f679
imcauan <cauanrodiniz@gmail.com>
2025-12-05 20:40:06 -0300

    chore: add Prettier and ESLint configuration files, update dependencies

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,39 +1,39 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-import unusedImports from "eslint-plugin-unused-imports";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js, "unused-imports": unusedImports },
-    extends: ["js/recommended"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js, 'unused-imports': unusedImports },
+    extends: ['js/recommended', 'plugin:prettier/recommended'],
     languageOptions: { globals: globals.node },
     rules: {
-      "no-unused-vars": "off",
-      "unused-imports/no-unused-imports": "error",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/explicit-module-boundary-types": "off",
-      "@typescript-eslint/no-empty-function": "off",
-      "@typescript-eslint/no-inferrable-types": "off",
-      "@typescript-eslint/ban-types": "off",
-      "@typescript-eslint/prefer-as-const": "error",
-      "@typescript-eslint/interface-name-prefix": "off",
-      "unused-imports/no-unused-vars": [
-        "warn",
+      'no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/prefer-as-const': 'error',
+      '@typescript-eslint/interface-name-prefix': 'off',
+      'unused-imports/no-unused-vars': [
+        'warn',
         {
-          vars: "all",
-          varsIgnorePattern: "^_",
-          args: "after-used",
-          argsIgnorePattern: "^_",
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
         },
       ],
     },
   },
   {
-    files: ["**/*.spec.ts", "**/*.test.ts", "**/*.spec.ts", "**/*.test.ts"],
+    files: ['**/*.spec.ts', '**/*.test.ts', '**/*.spec.ts', '**/*.test.ts'],
 
     languageOptions: {
       globals: {
@@ -42,8 +42,8 @@ export default defineConfig([
     },
 
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "no-console": "off",
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-console': 'off',
     },
   },
   tseslint.configs.recommended,
