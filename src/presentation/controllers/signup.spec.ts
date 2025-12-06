88f23046b2a194c0f6095e21abc61847ccf26ef5
imcauan <cauanrodiniz@gmail.com>
2025-12-06 19:46:20 -0300

    refactor: implement MissingParamError for clearer error handling in SignUpController

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,5 @@
 import { SignUpController } from './signup';
+import { MissingParamError } from '../errors/missing-param-error';
 
 describe('SignUp Controller', () => {
   it('should return 400 if no name is provided', () => {
@@ -17,7 +18,7 @@ describe('SignUp Controller', () => {
 
     // Assert
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'));
+    expect(httpResponse.body).toEqual(new MissingParamError('name'));
   });
 
   it('should return 400 if no email is provided', () => {
@@ -36,6 +37,6 @@ describe('SignUp Controller', () => {
 
     // Assert
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+    expect(httpResponse.body).toEqual(new MissingParamError('email'));
   });
 });
