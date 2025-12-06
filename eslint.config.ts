08c73c0e693d3f28736cdc1f9e3d6adfc4745a94
imcauan <cauanrodiniz@gmail.com>
2025-12-06 18:40:56 -0300

    fix: update ESLint configuration to improve TypeScript rules and plugin usage

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,17 +3,19 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 import unusedImports from 'eslint-plugin-unused-imports';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default defineConfig([
+  tseslint.configs.recommended,
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     plugins: { js, 'unused-imports': unusedImports },
-    extends: ['js/recommended', 'plugin:prettier/recommended'],
+    extends: ['js/recommended', eslintPluginPrettierRecommended],
     languageOptions: { globals: globals.node },
     rules: {
       'no-unused-vars': 'off',
       'unused-imports/no-unused-imports': 'error',
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-empty-function': 'off',
@@ -21,6 +23,7 @@ export default defineConfig([
       '@typescript-eslint/ban-types': 'off',
       '@typescript-eslint/prefer-as-const': 'error',
       '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
       'unused-imports/no-unused-vars': [
         'warn',
         {
@@ -46,5 +49,4 @@ export default defineConfig([
       'no-console': 'off',
     },
   },
-  tseslint.configs.recommended,
 ]);
