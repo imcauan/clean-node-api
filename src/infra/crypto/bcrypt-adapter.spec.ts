3db80fb7d5056c09ae85cff7fc43c2fa6ef80880
imcauan <cauanrodiniz@gmail.com>
2025-12-09 08:20:06 -0300

    refactor: add AAA missing comments for clarity in BcryptAdapter tests

--- src/infra/crypto/bcrypt-adapter.spec.ts
+++ src/infra/crypto/bcrypt-adapter.spec.ts
@@ -20,31 +20,39 @@ function makeSut(): SutTypes {
 
 describe('Bcrypt Adapter', () => {
   it('should call bcrypt with correct values', async () => {
+    // Arrange
     const { sut } = makeSut();
     const hashSpy = jest.spyOn(bcrypt, 'hash');
 
+    // Act
     await sut.encrypt('any_value');
 
+    // Assert
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
   });
 
   it('should return a hash on success', async () => {
+    // Arrange
     const { sut } = makeSut();
 
+    // Act
     const result = await sut.encrypt('any_value');
 
+    // Assert
     expect(result).toBe('hashed_value');
   });
 
   it('should throw if bcrypt throws', async () => {
+    // Arrange
     const { sut } = makeSut();
-
     jest
       .spyOn(bcrypt, 'hash')
       .mockReturnValue(Promise.reject(new Error()) as any);
 
+    // Act
     const result = sut.encrypt('any_value');
 
+    // Assert
     expect(result).rejects.toThrow();
   });
 });
