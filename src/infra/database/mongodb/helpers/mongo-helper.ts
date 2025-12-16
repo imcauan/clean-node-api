dd460493926bf12a887745cd04b1dcddff71cb48
imcauan <cauanrodiniz@gmail.com>
2025-12-16 08:32:03 -0300

    feat: implement MongoHelper connection and disconnection tests

--- src/infra/database/mongodb/helpers/mongo-helper.ts
+++ src/infra/database/mongodb/helpers/mongo-helper.ts
@@ -4,13 +4,21 @@ type MongoHelperMapCollection<T> = T & { _id: ObjectId };
 
 export const MongoHelper = {
   client: null as MongoClient | null,
+  url: null as string | null,
+
   async connect(url: string): Promise<void> {
+    this.url = url;
     this.client = await MongoClient.connect(url);
   },
   async disconnect(): Promise<void> {
     await this.client?.close();
+    this.client = null;
   },
   async getCollection(name: string): Promise<Collection> {
+    if (!this.client) {
+      await this.connect(this.url);
+    }
+
     return this.client.db().collection(name);
   },
   map<T>(collection: MongoHelperMapCollection<T>) {
