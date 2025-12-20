8035a9e1bc47355db0a1efc3f9e68cd9546976ce
Cauan Diniz <cauanrodiniz@gmail.com>
2025-12-20 10:02:34 -0300

    refactor: enhance test readability by adding Arrange, Act, and Assert comments

--- src/presentation/controllers/login.spec.ts
+++ src/presentation/controllers/login.spec.ts
@@ -58,6 +58,7 @@ function makeSut(): SutTypes {
 
 describe('Login Controller', () => {
   it('should return 400 if no email is provided', async () => {
+    // Arrange
     const { sut } = makeSut();
     const httpRequest = {
       body: {
@@ -65,12 +66,15 @@ describe('Login Controller', () => {
       },
     };
 
+    // Act
     const httpResponse = await sut.handle(httpRequest);
 
+    // Assert
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')));
   });
 
   it('should return 400 if no password is provided', async () => {
+    // Arrange
     const { sut } = makeSut();
     const httpRequest = {
       body: {
@@ -78,74 +82,97 @@ describe('Login Controller', () => {
       },
     };
 
+    // Act
     const httpResponse = await sut.handle(httpRequest);
 
+    // Assert
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')));
   });
 
   it('should call EmailValidator with correct email', async () => {
+    // Arrange
     const { sut, emailValidatorStub } = makeSut();
     const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid');
 
+    // Act
     await sut.handle(makeFakeRequest());
 
+    // Assert
     expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com');
   });
 
   it('should return 400 if an invalid email is provided', async () => {
+    // Arrange
     const { sut, emailValidatorStub } = makeSut();
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
 
+    // Act
     const httpResponse = await sut.handle(makeFakeRequest());
 
+    // Assert
     expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')));
   });
 
   it('should return 500 if EmailValidator throws', async () => {
+    // Arrange
     const { sut, emailValidatorStub } = makeSut();
     jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
       throw new Error();
     });
 
+    // Act
     const httpResponse = await sut.handle(makeFakeRequest());
 
+    // Assert
     expect(httpResponse).toEqual(serverError(new Error()));
   });
 
   it('should call authentication with correct values', async () => {
+    // Arrange
     const { sut, authenticationStub } = makeSut();
     const authSpy = jest.spyOn(authenticationStub, 'auth');
 
+    // Act
     await sut.handle(makeFakeRequest());
 
+    // Assert
     expect(authSpy).toHaveBeenCalledWith('any_email@mail.com', 'any_password');
   });
 
   it('should return 401 if invalid credentials are provided', async () => {
+    // Arrange
     const { sut, authenticationStub } = makeSut();
     jest
       .spyOn(authenticationStub, 'auth')
       .mockReturnValueOnce(Promise.resolve(null));
 
+    // Act
     const httpResponse = await sut.handle(makeFakeRequest());
 
+    // Assert
     expect(httpResponse).toEqual(unauthorized());
   });
 
   it('should return 500 if Authentication throws', async () => {
+    // Arrange
     const { sut, authenticationStub } = makeSut();
     jest.spyOn(authenticationStub, 'auth').mockRejectedValueOnce(new Error());
 
+    // Act
     const httpResponse = await sut.handle(makeFakeRequest());
 
+    // Assert
     expect(httpResponse).toEqual(serverError(new Error()));
   });
 
   it('should return 200 if valid credentials are provided', async () => {
+    // Arrange
     const { sut } = makeSut();
 
+    // Act
     const httpResponse = await sut.handle(makeFakeRequest());
 
+    // Assert
     expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }));
   });
 });
