090533e12d652fe423edb90f7af0a4289f1c2833
Cauan Diniz <cauanrodiniz@gmail.com>
2025-12-22 08:30:10 -0300

    test: ensure RequiredFieldValidation returns nothing if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -14,4 +14,17 @@ describe('RequiredFieldValidation', () => {
     // Assert
     expect(error).toEqual(new MissingParamError('any_field'));
   });
+
+  it('should not return if validation succeeds', () => {
+    // Arrange
+    const sut = new RequiredFieldValidation('any_field');
+
+    // Act
+    const error = sut.validate({
+      any_field: 'any_value',
+    });
+
+    // Assert
+    expect(error).toBeFalsy();
+  });
 });
