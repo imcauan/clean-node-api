fd68078594a7a78d25c2f775141418625b8d4e01
Cauan Diniz <cauanrodiniz@gmail.com>
2025-12-29 08:53:41 -0300

    feat: ensure JwtAdapter returns a token on sign success

--- src/infra/jwt/jwt-adapter.ts
+++ src/infra/jwt/jwt-adapter.ts
@@ -5,7 +5,7 @@ export class JwtAdapter implements TokenEncrypter {
   constructor(private readonly secret: string) {}
 
   async encrypt(id: string): Promise<string> {
-    jwt.sign({ id }, this.secret);
-    return null;
+    const accessToken = jwt.sign({ id }, this.secret);
+    return accessToken;
   }
 }
