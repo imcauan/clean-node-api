3d5d1383aa28f8a3391230ec6f9524b4db9089f7
Cauan Diniz <cauanrodiniz@gmail.com>
2025-12-30 07:52:37 -0300

    refactor: rename signup routes to auth routes

--- src/main/routes/auth-routes.test.ts
+++ src/main/routes/auth-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import app from '../config/app';
 import { MongoHelper } from '../../infra/database/mongodb/helpers/mongo-helper';
 
-describe('Signup Routes', () => {
+describe('Auth Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
   });
@@ -16,15 +16,17 @@ describe('Signup Routes', () => {
     await MongoHelper.disconnect();
   });
 
-  it('should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Cauan',
-        email: 'cauanrodiniz@gmail.com',
-        password: '123',
-        passwordConfirmation: '123',
-      })
-      .expect(200);
+  describe('POST /signup', () => {
+    it('should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Cauan',
+          email: 'cauanrodiniz@gmail.com',
+          password: '123',
+          passwordConfirmation: '123',
+        })
+        .expect(200);
+    });
   });
 });
