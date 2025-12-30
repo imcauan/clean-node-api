f3c5a5f258b16ba97fb9fafbdc76ae9ba06fd154
Cauan Diniz <cauanrodiniz@gmail.com>
2025-12-30 08:08:38 -0300

    test: ensure login route returns 401 with invalid credentials

--- src/main/routes/auth-routes.test.ts
+++ src/main/routes/auth-routes.test.ts
@@ -49,5 +49,14 @@ describe('Auth Routes', () => {
         })
         .expect(200);
     });
+    it('should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'cauanrodiniz@gmail.com',
+          password: '123',
+        })
+        .expect(401);
+    });
   });
 });
