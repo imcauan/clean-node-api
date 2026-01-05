eb5866eaf01ddb045789bc4f2127636c73543df0
Cauan Diniz <cauanrodiniz@gmail.com>
2026-01-05 07:24:17 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct accessToken

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,15 +1,52 @@
-import { AccessDeniedError, forbidden } from '@/presentation';
-import { AuthMiddleware } from '@/presentation/middlewares/auth-middleware';
+import { AccountModel, LoadAccountByToken } from '@/domain';
+import { AccessDeniedError, forbidden, AuthMiddleware } from '@/presentation';
+
+function makeFakeAccountData(): AccountModel {
+  return {
+    id: 'any_id',
+    email: 'any_email@mail.com',
+    name: 'any_name',
+    password: 'hashed_password',
+  };
+}
+
+function makeLoadAccountByToken() {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load(accessToken: string, role?: string): Promise<AccountModel> {
+      return makeFakeAccountData();
+    }
+  }
+
+  return new LoadAccountByTokenStub();
+}
 
 describe('Auth Middleware', () => {
   it('should return 403 if authorization does not exist in headers', async () => {
     // Arrange
-    const sut = new AuthMiddleware();
+    const loadAccountByTokenStub = makeLoadAccountByToken();
+    const sut = new AuthMiddleware(loadAccountByTokenStub);
 
     // Act
     const result = await sut.handle({});
 
     // Assert
     expect(result).toEqual(forbidden(new AccessDeniedError()));
+  });
+
+  it('should call LoadAccountByToken with correct accessToken', async () => {
+    // Arrange
+    const loadAccountByTokenStub = makeLoadAccountByToken();
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load');
+    const sut = new AuthMiddleware(loadAccountByTokenStub);
+
+    // Act
+    await sut.handle({
+      headers: {
+        authorization: 'any_token',
+      },
+    });
+
+    // Assert
+    expect(loadSpy).toHaveBeenCalledWith('any_token');
   });
 });
